0fae44131ca8ed17f007f4cddc860b01c4279112
bortcrab <bortcrab@gmail.com>
2024-02-23 20:22:27 -0700

    Retoques equis

--- NetBeans Projects/EjemploJPA_sin_JoinTable/src/main/java/com/mycompany/ejemplojpa_sin_jointable/EjemploJPA_Sin_JoinTable.java
+++ NetBeans Projects/EjemploJPA_sin_JoinTable/src/main/java/com/mycompany/ejemplojpa_sin_jointable/EjemploJPA_Sin_JoinTable.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.ejemplojpa_sinjointable;
+package com.mycompany.ejemplojpa_sin_jointable;
 
 import daos.JuegoDAO;
 import daos.JugadorDAO;
