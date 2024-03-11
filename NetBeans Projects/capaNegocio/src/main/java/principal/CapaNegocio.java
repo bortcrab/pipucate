58a7a1cd0cd650464a5a1f62a4fd263f13f1acfa
Juvee85 <juventino.lopez248547@potros.itson.edu.mx>
2024-03-11 16:50:09 -0700

    Creación de proyectos que manejan las capas de presentación y negocio

---
+++ NetBeans Projects/capaNegocio/src/main/java/principal/CapaNegocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package principal;
+
+/**
+ *
+ * @author Juventino López García - 00000248547
+ */
+public class CapaNegocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ NetBeans Projects/capaPresentacion/src/main/java/CapaPresentacion.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Juventino López García - 00000248547
+ */
+public class CapaPresentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
